ae311ba71e15eeca1b8e2849548ee1629451f4a3
Fabio Ribeiro <fabiorphp@gmail.com>
2018-06-17 11:19:40 -0300

    update: Update Node docs and sort struct fields
    Update documentation and sort the struct fields by size, following
    the linter.

--- node.go
+++ node.go
@@ -14,10 +14,10 @@ const (
 type (
 	// Node retrieves the info about the server nodes.
 	Node interface {
-		// Info retrieves the information about the server node
+		// Info retrieves the information about the server node.
 		Info() (*NodeInfo, *http.Response, error)
 
-		// InfoWithContext retrieves the information about the server node
+		// InfoWithContext retrieves the information about the server node.
 		InfoWithContext(ctx context.Context) (*NodeInfo, *http.Response, error)
 
 		// Status retrieves the status of the server node.
@@ -27,14 +27,13 @@ type (
 		StatusWithContext(ctx context.Context) (*NodeStatus, *http.Response, error)
 	}
 
-	// NodeService it's a concrete instance of node
+	// NodeService it's a concrete instance of node.
 	NodeService struct {
-
-		// Kongo client manages communication by API.
+		// Kongo client manages communication throught API.
 		client *Kongo
 	}
 
-	// NodeInfo it's a structure of API result
+	// NodeInfo it's a structure of API result.
 	NodeInfo struct {
 		Configuration *NodeInfoConfiguration `json:"configuration"`
 		Hostname      string                 `json:"hostname"`
@@ -46,35 +45,30 @@ type (
 		Version       string                 `json:"version"`
 	}
 
-	// NodeInfoConfiguration it's a structure of API result
+	// NodeInfoConfiguration it's a structure of API result.
 	NodeInfoConfiguration struct {
 		AdminAcessLog                 string              `json:"admin_access_log"`
 		AdminErrorLog                 string              `json:"admin_error_log"`
-		AdminListen                   []string            `json:"admin_listen"`
-		AdminListeners                []*NodeInfoListener `json:"admin_listeners"`
 		AdminSSLCertificateDefault    string              `json:"admin_ssl_cert_default"`
 		AdminSSLCertificateCsrDefault string              `json:"admin_ssl_cert_csr_default"`
 		AdminSSLCertificateKeyDefault string              `json:"admin_ssl_cert_key_default"`
-		AdminSSLEnabled               bool                `json:"admin_ssl_enabled"`
-		AnonymousReports              bool                `json:"anonymous_reports"`
+		AdminListen                   []string            `json:"admin_listen"`
+		AdminListeners                []*NodeInfoListener `json:"admin_listeners"`
 
 		CassandraConsistency            string   `json:"cassandra_consistency"`
-		CassandraContactPoints          []string `json:"cassandra_contact_points"`
-		CassandraDataCenters            []string `json:"cassandra_data_centers"`
 		CassandraKeyspace               string   `json:"cassandra_keyspace"`
 		CassandraLBPolicy               string   `json:"cassandra_lb_policy"`
+		CassandraReplicationStrategy    string   `json:"cassandra_repl_strategy"`
+		CassandraUsername               string   `json:"cassandra_username"`
+		CassandraContactPoints          []string `json:"cassandra_contact_points"`
+		CassandraDataCenters            []string `json:"cassandra_data_centers"`
 		CassandraPort                   int      `json:"cassandra_port"`
 		CassandraReplicationFactor      int      `json:"cassandra_repl_factor"`
-		CassandraReplicationStrategy    string   `json:"cassandra_repl_strategy"`
 		CassandraSchemaConsensusTimeout int      `json:"cassandra_schema_consensus_timeout"`
-		CassandraSSL                    bool     `json:"cassandra_ssl"`
-		CassandraSSLVerify              bool     `json:"cassandra_ssl_verify"`
 		CassandraTimeout                int      `json:"cassandra_timeout"`
-		CassandraUsername               string   `json:"cassandra_username"`
 
 		ClientBodyBufferSize           string `json:"client_body_buffer_size"`
 		ClientMaxBodySize              string `json:"client_max_body_size"`
-		ClientSSL                      bool   `json:"client_ssl"`
 		ClientSSLCertificateCsrDefault string `json:"client_ssl_cert_csr_default"`
 		ClientSSLCertificateDefault    string `json:"client_ssl_cert_default"`
 		ClientSSLCertificateKeyDefault string `json:"client_ssl_cert_key_default"`
@@ -86,19 +80,16 @@ type (
 		DatabaseUpdateFrequency   int    `json:"db_update_frequency"`
 		DatabaseUpdatePropagation int    `json:"db_update_propagation"`
 
-		DNSErrorTTL    int         `json:"dns_error_ttl"`
 		DNSHostsFile   string      `json:"dns_hostsfile"`
-		DNSNotFoundTTL int         `json:"dns_not_found_ttl"`
-		DNSNoSync      bool        `json:"dns_no_sync"`
 		DNSOrder       []string    `json:"dns_order"`
 		DNSResolver    interface{} `json:"dns_resolver"`
+		DNSNotFoundTTL int         `json:"dns_not_found_ttl"`
+		DNSErrorTTL    int         `json:"dns_error_ttl"`
 		DNSStaleTTL    int         `json:"dns_stale_ttl"`
 
 		ErrorDefaultType string `json:"error_default_type"`
 
 		KongEnv string `json:"kong_env"`
-
-		LatencyTokens bool `json:"latency_tokens"`
 
 		LuaPackageCPath   string `json:"lua_package_cpath"`
 		LuaPackagePath    string `json:"lua_package_path"`
@@ -115,31 +106,25 @@ type (
 		NginxDaemon          string `json:"nginx_daemon"`
 		NginxErrorLogs       string `json:"nginx_err_logs"`
 		NginxKongConf        string `json:"nginx_kong_conf"`
-		NginxOptimizations   bool   `json:"nginx_optimizations"`
 		NginxPID             string `json:"nginx_pid"`
 		NginxWorkerProcesses string `json:"nginx_worker_processes"`
 
 		Plugins map[string]bool `json:"plugins"`
 
-		PostgresDatabase  string `json:"pg_database"`
-		PostgresHost      string `json:"pg_host"`
-		PostgresPort      int    `json:"pg_port"`
-		PostgresSSL       bool   `json:"pg_ssl"`
-		PostgresUsername  string `json:"pg_user"`
-		PostgresSSLVerify bool   `json:"pg_ssl_verify"`
+		PostgresDatabase string `json:"pg_database"`
+		PostgresHost     string `json:"pg_host"`
+		PostgresUsername string `json:"pg_user"`
+		PostgresPort     int    `json:"pg_port"`
 
 		Prefix string `json:"prefix"`
 
-		ProxyAccessLog  string              `json:"proxy_access_log"`
-		ProxyErrorLog   string              `json:"proxy_error_log"`
-		ProxyListen     []string            `json:"proxy_listen"`
-		ProxyListeners  []*NodeInfoListener `json:"proxy_listeners"`
-		ProxySSLEnabled bool                `json:"proxy_ssl_enabled"`
+		ProxyAccessLog string              `json:"proxy_access_log"`
+		ProxyErrorLog  string              `json:"proxy_error_log"`
+		ProxyListen    []string            `json:"proxy_listen"`
+		ProxyListeners []*NodeInfoListener `json:"proxy_listeners"`
 
-		RealIpHeader    string `json:"real_ip_header"`
-		RealIpRecursive string `json:"real_ip_recursive"`
-
-		ServerTokens bool `json:"server_tokens"`
+		RealIPHeader    string `json:"real_ip_header"`
+		RealIPRecursive string `json:"real_ip_recursive"`
 
 		SSLCertificate           string `json:"ssl_cert"`
 		SSLCertificateDefault    string `json:"ssl_cert_default"`
@@ -149,45 +134,67 @@ type (
 		SSLCiphers               string `json:"ssl_ciphers"`
 		SSLCipherSuite           string `json:"ssl_cipher_suite"`
 
-		TrustedIps interface{} `json:"trusted_ips"`
+		TrustedIPs interface{} `json:"trusted_ips"`
 
 		UpstreamKeepAlive int `json:"upstream_keepalive"`
+
+		AdminSSLEnabled bool `json:"admin_ssl_enabled"`
+
+		AnonymousReports bool `json:"anonymous_reports"`
+
+		ClientSSL bool `json:"client_ssl"`
+
+		CassandraSSL       bool `json:"cassandra_ssl"`
+		CassandraSSLVerify bool `json:"cassandra_ssl_verify"`
+
+		DNSNoSync bool `json:"dns_no_sync"`
+
+		LatencyTokens bool `json:"latency_tokens"`
+
+		NginxOptimizations bool `json:"nginx_optimizations"`
+
+		PostgresSSL       bool `json:"pg_ssl"`
+		PostgresSSLVerify bool `json:"pg_ssl_verify"`
+
+		ProxySSLEnabled bool `json:"proxy_ssl_enabled"`
+
+		ServerTokens bool `json:"server_tokens"`
 	}
 
-	// NodeInfoListener it's a structure of API result
+	// NodeInfoListener it's a structure of API result.
 	NodeInfoListener struct {
-		SSL      bool   `json:"ssl"`
-		Ip       string `json:"ip"`
-		Protocol bool   `json:"protocol"`
-		Port     int    `json:"port"`
-		Http2    bool   `json:"http2"`
+		IP       string `json:"ip"`
 		Listener string `json:"listener"`
+		Port     int    `json:"port"`
+		Protocol bool   `json:"protocol"`
+		SSL      bool   `json:"ssl"`
+		HTTP2    bool   `json:"http2"`
 	}
 
-	// NodeInfoPlugins it's a structure of API result
+	// NodeInfoPlugins it's a structure of API result.
 	NodeInfoPlugins struct {
 		AvailableOnServer map[string]bool `json:"available_on_server"`
 		EnabledInCluster  []string        `json:"enabled_in_cluster"`
 	}
 
-	// NodeInfoTimers it's a structure of API result
+	// NodeInfoTimers it's a structure of API result.
 	NodeInfoTimers struct {
 		Pending int `json:"pending"`
 		Running int `json:"running"`
 	}
 
-	// NodeStatus it's a structure of API result
+	// NodeStatus it's a structure of API result.
 	NodeStatus struct {
 		Database *NodeStatusDatabase `json:"database"`
 		Server   *NodeStatusServer   `json:"server"`
 	}
 
-	// NodeStatusDatabase it's a structure of API result
+	// NodeStatusDatabase it's a structure of API result.
 	NodeStatusDatabase struct {
 		Reachable bool `json:"reachable, omitempty"`
 	}
 
-	// NodeStatusServer it's a structure of API result
+	// NodeStatusServer it's a structure of API result.
 	NodeStatusServer struct {
 		ConnectionsAccepted int `json:"connections_accepted, omitempty"`
 		ConnectionsActive   int `json:"connections_active, omitempty"`
@@ -199,7 +206,7 @@ type (
 	}
 )
 
-// InfoWithContext retrieves the server node information
+// InfoWithContext retrieves the server node information.
 func (n *NodeService) InfoWithContext(ctx context.Context) (*NodeInfo, *http.Response, error) {
 	resource, _ := url.Parse(nodeInfoResourcePath)
 
@@ -220,7 +227,7 @@ func (n *NodeService) InfoWithContext(ctx context.Context) (*NodeInfo, *http.Res
 	return nodeInfo, res, nil
 }
 
-// Info retrieves the server node information
+// Info retrieves the server node information.
 func (n *NodeService) Info() (*NodeInfo, *http.Response, error) {
 	return n.InfoWithContext(context.TODO())
 }
